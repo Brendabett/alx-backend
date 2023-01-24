cadef87a2793a8555dc04488b400dd0bc0ff4732
Brendabett <98813715+Brendabett@users.noreply.github.com>
2023-01-24 12:16:16 +0300

    Basic dictionary

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ Basic dictionary
 """
-BaseCaching = __import__(base_caching).BaseCaching
+BaseCaching = __import__('base_caching').BaseCaching
 
 
 class BasicCache(BaseCaching):
