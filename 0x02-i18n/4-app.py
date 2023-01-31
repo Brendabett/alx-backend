db87bdaf3f6856f50a7088feca596d543f1fa646
Brendabett <brendabett33@gmail.com>
2023-01-31 05:51:57 -0800

    Force locale with URL parameter

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -15,9 +15,9 @@ class Config:
     """
     Config class
     """
-    LANGUAGES = ['en', 'fr']
-    BABEL_DEFAULT_LOCALE = 'en'
-    BABEL_DEFAULT_TIMEZONE = 'UTC'
+    LANGUAGES = [en, fr]
+    BABEL_DEFAULT_LOCALE = en
+    BABEL_DEFAULT_TIMEZONE = UTC
 
 
 app.config.from_object(Config)
@@ -29,19 +29,19 @@ def get_locale():
     detect if the incoming request contains locale
     argument and ifs value is a supported locale, return it
     """
-    locale = request.args.get('locale')
+    locale = request.args.get(locale)
     if locale:
         return locale
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
+    return request.accept_languages.best_match(app.config[LANGUAGES])
 
 
-@app.route('/', methods=['GET'], strict_slashes=False)
+@app.route(/, methods=[GET], strict_slashes=False)
 def index():
     """
     hello world
     """
-    return render_template('4-index.html')
+    return render_template(4-index.html)
 
 
-if __name__ == '__main__':
+if __name__ == __main__:
     app.run(debug=True)
