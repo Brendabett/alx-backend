0da86f931c049790eb3304c1d82ed7d7cd3f5eb7
Brendabett <brendabett33@gmail.com>
2023-01-31 07:34:50 -0800

    My commit

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -1,11 +1,26 @@
 #!/usr/bin/env python3
-""" Entry point for the application. """
+
+"""
+5. Basic Flask app
+"""
 
 from flask import Flask, render_template, request, g
 from flask_babel import Babel
 
 app = Flask(__name__)
 babel = Babel(app)
+
+
+class Config:
+    """
+    Config class.
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
 
 users = {
     1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
@@ -15,63 +30,42 @@ users = {
 }
 
 
-@babel.localeselector
-def get_locale():
-    """ Locale selector. """
-    if request.args.get('locale') in Config.LANGUAGES:
-        return request.args.get('locale')
-
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
-
-
-def get_user() -> dict:
+def get_user(login_as):
     """
-    If the login_as query parameter is present,
-    return the user with the corresponding id, otherwise
-    return None
-    :return: A dictionary of the user.
+    get_user.
     """
-    login_as = request.args.get('login_as', False)
-    if login_as:
-        user = users[int(login_as)]
-        if user:
-            return user
-    return None
-
-
-class Config():
-    """ The `Config` class is a class that contains a list of languages"""
-    LANGUAGES = ['en', 'fr']
-    BABEL_DEFAULT_LOCALE = 'en'
-    BABEL_DEFAULT_TIMEZONE = 'UTC'
-
-
-app.config.from_object(Config)
+    try:
+        return users.get(int(login_as))
+    except Exception:
+        return
 
 
 @app.before_request
 def before_request():
     """
-    > If the user is logged in, get the user object from
-    the database and store it in the global
-    variable `g.user`
+    before_request
     """
-
-    if get_user():
-        g.user = get_user()
+    g.user = get_user(request.args.get("login_as"))
 
 
-@app.route('/', strict_slashes=False)
-def index() -> str:
+@babel.localeselector
+def get_locale():
     """
-    The function `index()` returns the rendered template `index.html`
-    :return: The index.html file is being returned.
+    get_locale.
     """
+    locale = request.args.get("locale")
+    if locale:
+        return locale
+    return request.accept_languages.best_match(app.config[LANGUAGES])
 
-    return render_template('5-index.html')
+
+@app.route(/, methods=["GET"], strict_slashes=False)
+def hello():
+    """
+    hello.
+    """
+    return render_template(5-index.html)
 
 
 if __name__ == "__main__":
-    """ This is a common Python idiom to check if the file
-    is being run as a script or imported as a module."""
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host="0.0.0.0", port="5000")
